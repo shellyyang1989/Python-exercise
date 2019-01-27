889e1faa1675693062952e8ccc5ad3245810187f
shellyyang1989 <786634816@qq.com>
2019-01-27 21:40:35 +0800

    Practise to use unittest

---
+++ unittest_exercise.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 27 16:27:28 2019
+
+对Student类编写单元测试，结果发现测试不通过，请修改Student类，让测试通过
+
+@author: yangyang43
+"""
+import unittest
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def get_grade(self):
+        if self.score > 100 or self.score < 0:
+            raise ValueError('score = %d Score must be in 0~100.' % self.score)
+        elif self.score < 60:
+            return 'C'
+        elif self.score >= 80:
+            return 'A'
+        else:
+            return 'B'
+
+class TestStudent(unittest.TestCase):
+
+    def test_80_to_100(self):
+        s1 = Student('Bart', 80)
+        s2 = Student('Lisa', 100)
+        self.assertEqual(s1.get_grade(), 'A')
+        self.assertEqual(s2.get_grade(), 'A')
+
+    def test_60_to_80(self):
+        s1 = Student('Bart', 60)
+        s2 = Student('Lisa', 79)
+        self.assertEqual(s1.get_grade(), 'B')
+        self.assertEqual(s2.get_grade(), 'B')
+
+    def test_0_to_60(self):
+        s1 = Student('Bart', 0)
+        s2 = Student('Lisa', 59)
+        self.assertEqual(s1.get_grade(), 'C')
+        self.assertEqual(s2.get_grade(), 'C')
+
+    def test_invalid(self):
+        s1 = Student('Bart', -1)
+        s2 = Student('Lisa', 101)
+        with self.assertRaises(ValueError):
+            s1.get_grade()
+        with self.assertRaises(ValueError):
+            s2.get_grade()
+
+if __name__ == '__main__':
+    unittest.main()
